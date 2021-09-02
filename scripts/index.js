38c605514ddaa101a6e77b76f9453c736ea10a54
rrTio <tio.rozsraven@ue.edu.ph>
2021-09-02 16:29:59 +0800

    Update

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-var mp1Form = document.getElementById("formMP1");
-document.getElementById("btn_mp1").addEventListener("click", MP1(){ window.open('https://rrtio.github.io/BIO-NOTE', '_blank')});
-
-
-function MP2(){window.open('https://rrtio.github.io/BIO-NOTE-WITH-CSS/', '_blank');}
-
-function MP3(){}
